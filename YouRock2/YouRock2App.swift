da906519e0281083d01280e4092a81ce847149de
Matthew Vonderheide <mattvonderheide@yahoo.com>
2021-04-22 15:14:29 -0500

    Initial Commit

---
+++ YouRock2/YouRock2App.swift
@@ -0,0 +1,17 @@
+//
+//  YouRock2App.swift
+//  YouRock2
+//
+//  Created by admin on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct YouRock2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
